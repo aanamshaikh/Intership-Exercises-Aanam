5871d7bd8ec175f1b52bed844b73ab9ff0f23b14
aanamshaikh <aanamshaikh01@gmail.com>
2022-04-18 20:40:51 +0530

    Refactored Exercise 3

--- src/main/java/exercise_3/Grep.java
+++ src/main/java/exercise_3/Grep.java
@@ -15,16 +15,20 @@ import static java.lang.System.in;
 import static java.lang.System.out;
 
 public class Grep {
+
     public static void main(String[] args) throws IOException {
+
         searchRecursively("test", Paths.get("src/main/resources/"));
         List<String> a =Arrays.asList("literature a great subject");
         writeToFile(a, new File("src/main/resources/OutputFile.txt"));
-
     }
 
     public static List<String> searchStringInFile(String searchString, Path file) {
+
         try (Stream<String> lines = Files.lines(file)) {
-            return lines.filter(s -> s.contains(searchString)).collect(Collectors.toList());
+            return lines.filter(s -> s
+                    .contains(searchString))
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -37,9 +41,9 @@ public class Grep {
             {
                 if (Files.isRegularFile(f)) {
                     try {
-                        if(Files.isReadable(f)){
+                        if(Files.isReadable(f)) {
                             List<String> s =searchStringInFile(searchString,f);
-                            for (String line:s){
+                            for (String line:s) {
                                 System.out.println(f + " : "+line);
                             }
                         }
@@ -52,6 +56,7 @@ public class Grep {
             e.printStackTrace();
         }
     }
+
     public static List<String> extractedFromInput() {
         List<String> scList;
         try (Scanner sc = new Scanner(in)) {
@@ -60,11 +65,13 @@ public class Grep {
         }
         return scList;
     }
+
     public static List<String> searchProcessing(String searchString, List<String> result) {
         return result.stream()
                 .filter(line -> line.contains(searchString))
                 .collect(Collectors.toList());
     }
+
     public static List<String> writeToFile(List<String> list, File fileName) throws IOException {
 
 //        Files.write(Paths.get("src/main/java/exercise_3/OutputFile.txt"), list);
@@ -73,5 +80,4 @@ public class Grep {
         }
         return list;
     }
-
 }
--- src/test/java/exercise_3/MainTest.java
+++ src/test/java/exercise_3/MainTest.java
@@ -5,39 +5,39 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.CoreMatchers.is;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
-
-
 
 class MainTest {
 
      @Test
      public void testToSearchInFile() throws IOException {
-
          List<String> input = Grep.searchStringInFile("literature", Paths.get("src/main/resources/test1.txt"));
          List<String> writeFile = Grep.writeToFile(input, new File("src/main/java/exercise_3/OutputFile.txt"));
          MatcherAssert.assertThat(writeFile, is(writeFile));
-     }
-    @Test
-    public void recursiveSearch() {
 
+     }
+
+     @Test
+    public void recursiveSearch() {
         Grep.searchRecursively("test", Paths.get("src/main/resources/"));
-    }
+
+     }
+
     @Test
     public void writeToFile() throws IOException {
         List<String> list = Arrays.asList("Literature is a great subject");
         Grep.writeToFile(list, new File("src/main/resources/OutputFile.txt"));
-    }
+
+     }
+
     @Test
     public void testFromStdin() {
-
      List<String> inp =Arrays.asList("literature","lite","light","litter");
      List<String> output = Grep.searchProcessing("lit",inp);
      List<String> expectedOutput = Arrays.asList("literature","lite","litter");
      MatcherAssert.assertThat(output, is(expectedOutput));
-     }
 
+     }
 }
